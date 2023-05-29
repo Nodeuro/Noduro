04b63e350333fb8ad66bea0d2a3ac074d9e87ec7
Aadvik <vashist.aadvik@gmail.com>
2023-05-29 12:36:07 -0400

    updates updates updates

--- app/slidebar.js
+++ app/slidebar.js
@@ -2,10 +2,10 @@ var files = JSON.parse(localStorage.getItem("files"));
 var lessons = JSON.parse(localStorage.getItem("lessons"));
 
 for (index = 0; index < files.length; index++) {
-    add_element("recommended-div", lessons[index], files[index]);
-    add_element("saved-div", lessons[index], files[index]);
-    animations("recommended-div");
-    animations("saved-div");
+    add_element("recommended_div", lessons[index], files[index]);
+    add_element("saved_div", lessons[index], files[index]);
+    animations("recommended_div");
+    animations("saved_div");
 }
 
 function add_element(div_name, image_source, name) {
@@ -49,9 +49,9 @@ function animations(div_name) {
         // console.log("scrolling")
         // Check if the user has already scrolled
         // If this is the first time the user has scrolled, do something
-        // if (!document.getElementById("scrolling-div").classList.contains("open-scrolling-div")){
-        document.getElementById(div_name).classList.add("open-scrolling-div");
-        document.getElementById(div_name + "_recommendation_text").classList.add("text-opened");
+        // if (!document.getElementById("scrolling_div").classList.contains("open_scrolling_div")){
+        document.getElementById(div_name).classList.add("open_scrolling_div");
+        document.getElementById(div_name + "_recommendation_text").classList.add("text_opened");
         document.getElementById(div_name + "_left_shadow").classList.add("box_appear");
         document.getElementById(div_name + "_right_shadow").classList.add("box_appear");
         document.getElementById(div_name + "_left_shadow").classList.remove("before");
@@ -61,8 +61,8 @@ function animations(div_name) {
         setTimeout(function () {
             if (Date.now() - lastscroll > 10000) {
                 // console.log("setting back")
-                document.getElementById(div_name).classList.remove("open-scrolling-div");
-                document.getElementById(div_name + "_recommendation_text").classList.remove("text-opened");
+                document.getElementById(div_name).classList.remove("open_scrolling_div");
+                document.getElementById(div_name + "_recommendation_text").classList.remove("text_opened");
 
                 document.getElementById(div_name + "_left_shadow").classList.add("before");
                 document.getElementById(div_name + "_right_shadow").classList.add("before");
